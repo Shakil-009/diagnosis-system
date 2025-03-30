2c6a39076e7e12c48e2794cc022307fb5062cd62
Shakil-009 <kilsha009@gmail.com>
2025-03-30 16:48:05 +0600

    Added login section

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const login = () => {
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const specialChars = "._-+@";
+    if(typeof email !== 'string')
+    {
+        return '"Invalid"';
+    }
+    if(specialChars.includes(email[0]) || email.includes(" ") || !email.endsWith(".com") || typeof email !== 'string' || (email.match(/@/g) || []).length !== 1)
+    {
+        alert("Please put a valid email")
+    }
+
+    else {
+        if(password == 1234)
+            {
+                alert('Successfully logged in');
+            }
+            else{
+                alert("Wrong password");
+            }
+    }
+    
+}
